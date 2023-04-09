8ccf94655c3ec16cc10643446d38860bf64453f1
tlsalswo1004 <tlsalswo1004@naver.com>
2023-04-09 22:36:32 +0900

    db 수정

--- src/main/java/com/controller/BoardController.java
+++ src/main/java/com/controller/BoardController.java
@@ -17,120 +17,80 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardService bservice;
 
-	@Autowired
-	private BoardService bservice;
-	
-	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
-			
-	
-		/*게시글 목록 리스트*/
-		//@RequestMapping(value = "/list", method = RequestMethod.GET)
-		//public void listGET(Model model) throws Exception{
-				
-		/* logger.info("list 결과=" + bservice.boardList()); *///select 전체목록
-			//select 전체 목록을 Model객체에 저장후 list.jsp에 실어서 보낸다.
-		//	model.addAttribute("list", bservice.boardList());
-			
-			
-		/* return "board/list"; */
-				
-	//	}
-	
-	
-		//페이징된 게시글 목록 리스트
-		@RequestMapping(value = "/list", method = RequestMethod.GET)
-		public void listGET(Criteria cri, Model model) throws Exception{
-			int total = bservice.boardCount(cri);
-			PageVO pv = new PageVO(cri, total);
-			logger.info("keyword="+cri.getKeyword());
-			logger.info("tpta; = " + total);
-			logger.info("bservice.boardListPaging()" + bservice.boardListPaging(cri));
-			model.addAttribute("list",bservice.boardListPaging(cri));
-			model.addAttribute("page",  pv);
-			
-		
-				
-		}
-		
-		
-		
-		
-	
-		/* 글쓰기를 위한 controller */
-			@RequestMapping(value = "/register" , method = RequestMethod.GET)
-			public String registGET() throws Exception{
-				logger.info("regist get........");
-				return "board/write";
-			}
-			
-			@RequestMapping(value = "/register" ,  method = RequestMethod.POST)
-			public String registPOST(BoardVO board) throws Exception{
-				logger.info("regist post......."+board);
-				
-				bservice.boardWrite(board);
-					
-				
-				return "redirect:list";
-			}
-			
-			
-			@RequestMapping(value = "/detail", method = RequestMethod.GET)
-			public String detailGET(@RequestParam int bnum, Model model) throws Exception{
-				logger.info("detail GET......" + bnum);
-				model.addAttribute("bom", bservice.boardDetail(bnum));
-		 return "board/detail"; 
-			}
-		
-			
-			@RequestMapping(value = "/modify", method = RequestMethod.GET)
-			public String modifyGET(int bnum, Model model) throws Exception{
-				logger.info("modify GET......" + bnum);
-				model.addAttribute("bom", bservice.boardDetail(bnum));
-				
-		 return "board/modify"; 
-			}
-			
-			@RequestMapping(value = "/modify", method = RequestMethod.POST)
-			public String modifyPOST(BoardVO bom) throws Exception{
+    private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
-				logger.info("modify POST......");
-				bservice.boardModify(bom);
-				
-		 //return "redirect:list"; 
-				return "redirect:detail?bnum="+bom.getNumber(); 
-		 
-			}
-			/*@RequestMapping(value = "/modify", method = RequestMethod.POST)
-			public String modifyPOST(BoardVO bom,RedirectAttributes rttr) throws Exception{
-				logger.info("modify POST......");
-				bservice.boardModify(bom);
-				rttr.addFlashAttribute("bnum", bom.getNumber());
-				
-				return "redirect:detail";
-		 
-			}*/
-			
-			
-			
-		@RequestMapping(value = "/delete", method = RequestMethod.GET)
-		public String deleteGET(@RequestParam int bnum, Model model) throws Exception{
-				logger.info("delete GET......" + bnum);
-				bservice.boardDel(bnum);
-				
-		 return "redirect:list"; 
-		 
-			}
-	
+    //페이징된 게시글 목록 리스트
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public void listGET(Criteria cri, Model model) throws Exception {
+        int total = bservice.boardCount(cri);
+        PageVO pv = new PageVO(cri, total);
+        logger.info("keyword=" + cri.getKeyword());
+        logger.info("tpta; = " + total);
+        logger.info("bservice.boardListPaging()" + bservice.boardListPaging(cri));
+        model.addAttribute("list", bservice.boardListPaging(cri));
+        model.addAttribute("page", pv);
+    }
 
-	@RequestMapping(value = "/replyboard", method = RequestMethod.GET)
-	public void replyGET() throws Exception{
-		logger.info("replyboard GET......");
-		
-		//return "board/replyboard";
-	}
-	
-	
-	
-	
+    /* 글쓰기를 위한 controller */
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registGET() throws Exception {
+        logger.info("regist get........");
+        return "board/write";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String registPOST(BoardVO board) throws Exception {
+        logger.info("regist post......." + board);
+
+        bservice.boardWrite(board);
+
+        return "redirect:list";
+    }
+
+
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public String detailGET(@RequestParam int bnum, Model model) throws Exception {
+        logger.info("detail GET......" + bnum);
+        model.addAttribute("bom", bservice.boardDetail(bnum));
+        return "board/detail";
+    }
+
+
+    @RequestMapping(value = "/modify", method = RequestMethod.GET)
+    public String modifyGET(int bnum, Model model) throws Exception {
+        logger.info("modify GET......" + bnum);
+        model.addAttribute("bom", bservice.boardDetail(bnum));
+
+        return "board/modify";
+    }
+
+    @RequestMapping(value = "/modify", method = RequestMethod.POST)
+    public String modifyPOST(BoardVO bom) throws Exception {
+
+        logger.info("modify POST......");
+        bservice.boardModify(bom);
+
+        //return "redirect:list";
+        return "redirect:detail?bnum=" + bom.getNumber();
+
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public String deleteGET(@RequestParam int bnum, Model model) throws Exception {
+        logger.info("delete GET......" + bnum);
+        bservice.boardDel(bnum);
+
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/replyboard", method = RequestMethod.GET)
+    public void replyGET() throws Exception {
+        logger.info("replyboard GET......");
+
+        //return "board/replyboard";
+    }
+
 }
--- src/main/java/com/controller/ReplyController.java
+++ src/main/java/com/controller/ReplyController.java
@@ -23,125 +23,111 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/replies")
 public class ReplyController {
 
-	@Autowired
-	private ReplyService reservice;
-	
-	private static final Logger logger = LoggerFactory.getLogger(ReplyController.class);
-	
-	
-	@RequestMapping(value="", method=RequestMethod.POST)
-	public ResponseEntity<String> register(@RequestBody ReplyVO vo){
-		
-		logger.info("ReplyVO : " + vo);
-		ResponseEntity<String> entity = null;
-		try {
-			reservice.RepWrite(vo);
-			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
-		} catch(Exception e) {
-			e.printStackTrace();
-			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-		
-		return entity;
-	}
-	
-	// 댓글 리스트보기(페이징 x)
-	@RequestMapping(value="/all/{bno}", method=RequestMethod.GET)
-	public ResponseEntity<List<ReplyVO>> list(@PathVariable("bno") int bno){
-		logger.info("ReplyVO : " + bno);
-		ResponseEntity<List<ReplyVO>>entity=null;
-		try {
-			entity = new ResponseEntity<List<ReplyVO>>(reservice.RepList(bno),HttpStatus.OK); 
-		}catch(Exception e) {
-			e.printStackTrace();
-			entity = new ResponseEntity<List<ReplyVO>>(HttpStatus.BAD_REQUEST);
-		}
-		
-		return entity;
-		
-	}
-	
-	// 댓글 리스트보기(페이징 o)
-		@RequestMapping(value="/{bno}/{page}", method=RequestMethod.GET)
-		public ResponseEntity<Map<String,Object>> listPage(
-				@PathVariable("bno") int bno,
-				@PathVariable("page") int page){
-			
-			logger.info("ReplyVO : " + bno);
-			logger.info("ReplyVO : " + page);
-			ResponseEntity<Map<String,Object>> entity = null;
-			try {
-				Criteria cri = new Criteria();//페이징에 필요한 페이지 번호와, 한 페이지당 리스트 갯수,
-				cri.setPageNum(page);
-				
-				int cnt = reservice.RepCount(bno);
-				PageVO pv = new PageVO(cri, cnt);
-				
-				
-				Map<String, Object> map = new HashMap<String, Object>();
-				List<ReplyVO> list = reservice.ReplistPage(bno, cri);
-				
-				map.put("list",  list);// 게시판 리스트
-				map.put("pv", pv);// 페이징 관련
-				
-				entity = new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
-			} catch(Exception e) {
-				e.printStackTrace();
-				entity = new ResponseEntity<Map<String,Object>>(HttpStatus.BAD_REQUEST);
-			}
-			return entity;
-		}
+    @Autowired
+    private ReplyService reservice;
 
-	
-	
-	
-	@RequestMapping(value="/{rno}", method= {RequestMethod.PUT, RequestMethod.PATCH})
-	public ResponseEntity<String> update(@PathVariable("rno")Integer rno, @RequestBody ReplyVO vo){
-		
-		logger.info("ReplyVO : " + rno);
-		ResponseEntity<String> entity = null;
-		try {
-			vo.setRno(rno);;
-			reservice.RepModify(vo);
-			
-			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-		return entity;
-	
-	
-	
-	
-	}
-	
-	@RequestMapping(value="/{rno}", method= {RequestMethod.DELETE})
-	public ResponseEntity<String> remove(@PathVariable("rno")Integer rno){
-		
-		logger.info("ReplyVO : " + rno);
-		ResponseEntity<String> entity = null;
-		try {
-			reservice.RepDel(rno);
-			entity=new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
-		}catch(Exception e) {
-			e.printStackTrace();
-			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-		return entity;
-	}
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    private static final Logger logger = LoggerFactory.getLogger(ReplyController.class);
+
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<String> register(@RequestBody ReplyVO vo) {
+
+        logger.info("ReplyVO : " + vo);
+        ResponseEntity<String> entity = null;
+        try {
+            reservice.RepWrite(vo);
+            entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        return entity;
+    }
+
+    // 댓글 리스트보기(페이징 x)
+    @RequestMapping(value = "/all/{bno}", method = RequestMethod.GET)
+    public ResponseEntity<List<ReplyVO>> list(@PathVariable("bno") int bno) {
+        logger.info("ReplyVO : " + bno);
+        ResponseEntity<List<ReplyVO>> entity = null;
+        try {
+            entity = new ResponseEntity<List<ReplyVO>>(reservice.RepList(bno), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<List<ReplyVO>>(HttpStatus.BAD_REQUEST);
+        }
+
+        return entity;
+
+    }
+
+    // 댓글 리스트보기(페이징 o)
+    @RequestMapping(value = "/{bno}/{page}", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> listPage(
+            @PathVariable("bno") int bno,
+            @PathVariable("page") int page) {
+
+        logger.info("ReplyVO : " + bno);
+        logger.info("ReplyVO : " + page);
+        ResponseEntity<Map<String, Object>> entity = null;
+        try {
+            Criteria cri = new Criteria();//페이징에 필요한 페이지 번호와, 한 페이지당 리스트 갯수,
+            cri.setPageNum(page);
+
+            int cnt = reservice.RepCount(bno);
+            PageVO pv = new PageVO(cri, cnt);
+
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            List<ReplyVO> list = reservice.ReplistPage(bno, cri);
+
+            map.put("list", list);// 게시판 리스트
+            map.put("pv", pv);// 페이징 관련
+
+            entity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<Map<String, Object>>(HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
+
+
+    @RequestMapping(value = "/{rno}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+    public ResponseEntity<String> update(@PathVariable("rno") Integer rno, @RequestBody ReplyVO vo) {
+
+        logger.info("ReplyVO : " + rno);
+        ResponseEntity<String> entity = null;
+        try {
+            vo.setRno(rno);
+            ;
+            reservice.RepModify(vo);
+
+            entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+
+
+    }
+
+    @RequestMapping(value = "/{rno}", method = {RequestMethod.DELETE})
+    public ResponseEntity<String> remove(@PathVariable("rno") Integer rno) {
+
+        logger.info("ReplyVO : " + rno);
+        ResponseEntity<String> entity = null;
+        try {
+            reservice.RepDel(rno);
+            entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
+
+
 }
