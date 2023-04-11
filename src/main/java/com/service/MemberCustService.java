a86c245abd8b54e61838729b19efc2cb20831466
won <elven0480@naver.com>
2023-04-12 00:50:31 +0900

    서비스 오류수정

--- src/main/java/com/service/MemberCustService.java
+++ src/main/java/com/service/MemberCustService.java
@@ -3,7 +3,9 @@ package com.service;
 import com.google.gson.Gson;
 import com.mapper.MemberCustMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberCustService {
 
     @Autowired
